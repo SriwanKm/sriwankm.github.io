8171a9e1242c1110caa03de5cada5b0a951f9b8f
Sriwankm <kcqkoo@gmail.com>
2024-10-24 16:20:15 -0400

    added links

--- app/componants/ContactSection.tsx
+++ app/componants/ContactSection.tsx
@@ -9,12 +9,12 @@ import resumeIcon from "../public/resume.png"
 
 export default function ContactSection() {
     return (
-        <div className={`${styles.details} md:mb-20`}>
+        <div id="contact" className={`${styles.details} md:mb-20`}>
             <Image className="w-11/12 md:w-10/12 lg:w-7/12 mx-auto mb-8" src={getInTouchHeader} alt="getInTouch"></Image>
 
             <div className="bg-[#FFFFFF]/[.10] drop-shadow-[30px_20px_15px_rgba(0,0,0,0.3)] py-8 lg:py-16 md:pt-10 backdrop-brightness-110 backdrop-blur-xl lg:w-full mx-auto flex flex-col md:items-center">
 
-                <a href=""><div className="flex items-center mb-12 mx-10"> <Image className="w-8 mx-3" src={gmail} alt="gmail"></Image><p className="text-center">sriwan.km@gmail.com</p></div></a>
+                <a target="_blank" href="mailto:sriwan.km@gmail.com"><div className="flex items-center mb-12 mx-10"> <Image className="w-8 mx-3" src={gmail} alt="gmail"></Image><p className="text-center">sriwan.km@gmail.com</p></div></a>
                
                 <div className="block md:flex justify-center items-end mb-5">
                     <div className="mx-10 lg:mx-32 mb-12 md:mb-0">
@@ -24,7 +24,7 @@ export default function ContactSection() {
                     </div>
                     <div className="mx-10 lg:mx-32">
                         <div className="flex items-center"><Image className="w-7 mx-3" src={phone} alt="phone"></Image> <p>929 224 2141</p></div>
-                        <a href=""><div className="flex items-center mt-12"><Image className="w-8 mx-3" src={ig} alt="instagram"></Image> <p>Instagram</p></div></a>
+                        <a target="_blank" href="https://www.instagram.com/whatthebrain_nft"><div className="flex items-center mt-12"><Image className="w-8 mx-3" src={ig} alt="instagram"></Image> <p>Instagram</p></div></a>
 
                     </div>
                 </div>
--- app/componants/RetroButton.tsx
+++ app/componants/RetroButton.tsx
@@ -2,6 +2,6 @@ import styles from "../page.module.css"
 
 export default function RetroButton(props: {color: string, text: string}) {
     return (
-        <div className={`${styles.retro_button} ${props.color} text-center w-fit md:w-36 px-2 py-1 md:py-2.5 min-w-24 mx-auto border-solid border-2 border-[#020202] rounded-3xl drop-shadow-[-3px_4px_0px_rgba(2,2,2,1)] md:drop-shadow-[-3px_5px_0px_rgba(2,2,2,1)]`}>{props.text}</div>
+        <div className={`${styles.retro_button} ${props.color} text-center w-fit md:w-36 px-2 py-1 md:py-2.5 min-w-28 mx-auto border-solid border-2 border-[#020202] rounded-3xl drop-shadow-[-3px_4px_0px_rgba(2,2,2,1)] md:drop-shadow-[-3px_5px_0px_rgba(2,2,2,1)]`}>{props.text}</div>
     )
 }
