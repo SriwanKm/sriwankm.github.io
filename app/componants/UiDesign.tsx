70fc0706030ba0bdb28caa77180ddd8d1e31f240
Sriwankm <kcqkoo@gmail.com>
2024-11-18 17:35:14 -0500

    fixed image size in ui design section

--- app/componants/UiDesign.tsx
+++ app/componants/UiDesign.tsx
@@ -13,8 +13,8 @@ export default function UiDesign() {
             <div><Image className="w-3/4 md:w-1/2 lg:w-4/12 mx-auto my-7" src={uiHeader} alt="uiHeader"></Image></div>
 
             <div className="my-6 lg:mx-10 lg:flex">
-                <Image className="mx-auto mb-6 lg:mb-0 rounded-3xl w-11/12 lg:w-5/12 order-solid border-2 border-[#020202]" src={pollyApp} alt="pollyApp"></Image>
-                <Image className="mx-auto rounded-3xl w-11/12 lg:w-5/12 order-solid border-2 border-[#020202]" src={ganjaApp} alt="ganjaApp"></Image>
+                <Image className="mx-auto mb-6 lg:mb-0 rounded-3xl w-11/12 lg:w-2/5 order-solid border-2 border-[#020202]" src={pollyApp} alt="pollyApp"></Image>
+                <Image className="mx-auto rounded-3xl w-11/12 lg:w-2/5 order-solid border-2 border-[#020202]" src={ganjaApp} alt="ganjaApp"></Image>
 
             </div>
             {/* <div className="block lg:flex mb-24" id="ui-design"> */}
@@ -24,7 +24,7 @@ export default function UiDesign() {
                     <div className="mt-2">
                         <DetailsBox
                             text="I specialize in creating beautiful and functional digital experiences that are user-friendly and intuitive using various design tools, such as XD, and Figma, and I am well-versed in creating wireframes, prototypes, and high-fidelity mockups."
-                            attribute="w-11/12 lg:w-8/12 mx-auto"
+                            attribute="w-11/12 lg:w-10/12 mx-auto"
                         />
                         <RetroButton
                             text="Read more"
